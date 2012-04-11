94674b4669746b7206656c72e7a975362d7af3e2
Calenria <guildpress@novus-ordo-mundi.de>
2012-04-11 21:14:16 +0200

    Hotfix

--- app/jobs/Minute.java
+++ app/jobs/Minute.java
@@ -5,14 +5,27 @@ import java.util.List;
 import models.wowapi.character.Avatar;
 import models.wowapi.guild.GuildMember;
 import models.wowapi.resources.Recipe;
+import models.wowapi.resources.CharacterSpec;
+import models.wowapi.resources.Role;
 import play.Logger;
 import play.jobs.Every;
 import play.jobs.Job;
 
-@Every("5mn")
+@Every("2mn")
 public class Minute extends Job {
 	@Override
 	public void doJob() {
+
+
+		if (Role.findAll().size() == 0) {
+			Role.createRoles();
+		}
+		
+		if (CharacterSpec.findAll().size() == 0) {
+			CharacterSpec.createSpecs();
+		}
+
+
 		List<Recipe> recipes = Recipe.find("name is null").fetch(5);
 		if (recipes.size() > 0) {
 			Logger.info("[MinuteJob][setToolTip]");
